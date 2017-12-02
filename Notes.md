1956a94a77796eb2c31f57514896727e3027a54c
wasax9t <wasax9t@126.com>
2017-12-02 19:27:29 +0800

    Initial commit

---
+++ Notes.md
@@ -0,0 +1,6 @@
+## 是什么？
+SpringCloud不是一个项目，基于SpringBoot快速配置服务框架
+### Eureka
+服务中心
+### Hystrix
+熔断器，对一服务多次调用失败后直接默认失败
---
+++ eureka-consumer/src/main/java/com/wasax9t/HelloRemote.java
@@ -0,0 +1,11 @@
+package com.wasax9t;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "spring-cloud-producer")
+public interface HelloRemote {
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
